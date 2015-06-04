bf8cfd59b0deee213abefe49c9a734492b9b3a4a
Andrei Smirnov <pinebit@gmail.com>
2015-06-04 12:27:59 +0300

    Update Program.cs

--- MeetBondDemo/Program.cs
+++ MeetBondDemo/Program.cs
@@ -19,19 +19,24 @@
             
             Console.WriteLine("{0} events successfully read.", allEvents.Count);
 
+            // OutputBuffer will receive serialized data
             var output = new OutputBuffer();
+            // Choose another protocol writer to match your needs
             var writer = new SimpleBinaryWriter<OutputBuffer>(output);
 
             Console.Write("Serializing all records... ");
             Stopwatch stopwatch = new Stopwatch();
 
+            // Serialize.To performs actual serialization
             stopwatch.Start();
             allEvents.ForEach(x => Serialize.To(writer, x));
             stopwatch.Stop();
 
             Console.WriteLine("done in {0} ms, output size = {1} Kb", stopwatch.ElapsedMilliseconds, output.Position / 1024);
 
+            // InputBuffer represents octet stream source for deserialization
             var input = new InputBuffer(output.Data);
+            // Protocol reader shall match the writer from above
             var reader = new SimpleBinaryReader<InputBuffer>(input);
 
             Console.Write("Deserializing all records... ");
@@ -40,6 +45,7 @@
             stopwatch.Restart();
             for (int i = 0; i < allEvents.Count; i++)
             {
+                // Deserializing..
                 EventRecordData record = Deserialize<EventRecordData>.From(reader);
 
                 if (!counts.ContainsKey(record.Level))
